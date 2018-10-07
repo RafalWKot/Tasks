d3819f5bdccbd6bbcf961c47a0f5232f641cb009
RafalWKot <rafal.kot.poczta@gmail.com>
2018-10-08 01:58:23 +0200

    Add example and task from module 24.2
    Note:

--- src/main/java/com/crud/tasks/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/config/AdminConfig.java
@@ -10,4 +10,16 @@ public class AdminConfig {
 
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String company_name;
+
+    @Value("${info.company.email}")
+    private String company_email;
+
+    @Value("${info.company.phone}")
+    private String company_phone;
 }
